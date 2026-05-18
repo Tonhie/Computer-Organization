198c69b082fdca528d71a3694d64bc8dee85b369
Tonhie Xavier <tonhie.xavier@icloud.com>
2026-05-18 14:36:51 +0800

    add project cpu31

---
+++ Projects/CPU31/CPU31.srcs/sources_1/new/cpu.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/18/2026 02:36:35 PM
+// Design Name: 
+// Module Name: cpu
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module cpu(
+
+    );
+endmodule
